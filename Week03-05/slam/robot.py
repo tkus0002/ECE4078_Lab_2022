907726e6bf53255a8343dd7132351863c3d201d9
CharlieMonash <cdix0004@student.monash.edu>
2022-08-23 19:24:58 +1000

    eps include
    include eps

--- Week03-05/slam/robot.py
+++ Week03-05/slam/robot.py
@@ -77,8 +77,8 @@ class Robot:
         th = self.state[2]
         
         # TODO: add your codes here to compute DFx using lin_vel, ang_vel, dt, and th
-        DFx[0,2] = lin_vel / ang_vel * (-np.cos(th) + np.cos(th+ang_vel*dt))
-        DFx[1,2] = lin_vel / ang_vel * (-np.sin(th) + np.sin(th+ang_vel*dt))
+        DFx[0,2] = lin_vel / (ang_vel+np.finfo(float).eps) * (-np.cos(th) + np.cos(th+ang_vel*dt))
+        DFx[1,2] = lin_vel / (ang_vel+np.finfo(float).eps) * (-np.sin(th) + np.sin(th+ang_vel*dt))
 
         return DFx
 
