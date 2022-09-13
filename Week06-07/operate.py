3ab232b04c212c80eb8842701e6b4390674030bc
tkus0002 <tkus0002@student.monash.edu>
2022-09-13 19:35:51 +1000

    Added network weights and detector function

--- Week06-07/operate.py
+++ Week06-07/operate.py
@@ -301,7 +301,7 @@ if __name__ == "__main__":
     parser.add_argument("--calib_dir", type=str, default="calibration/param/")
     parser.add_argument("--save_data", action='store_true')
     parser.add_argument("--play_data", action='store_true')
-    parser.add_argument("--ckpt", default='network/scripts/model/model.best.pth')
+    parser.add_argument("--ckpt", default='network/scripts/model/best.pt')
     args, _ = parser.parse_known_args()
     
     pygame.font.init() 
